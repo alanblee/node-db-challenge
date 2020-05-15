f233883df96d9db23eef321b26a1cf7290c6c054
alanblee <alanbenlee12@gmail.com>
2020-05-15 08:04:22 -0700

    setup knex files

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+//import routes
+
+
+//config body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//config routes
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ knexfile.js
@@ -0,0 +1,33 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgres://localhost/sprint-project-db",
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
