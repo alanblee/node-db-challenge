d4d5546896a3353df701c0e868cef5bb1c5178f9
alanblee <alanbenlee12@gmail.com>
2020-05-17 10:42:14 -0700

    added a '/' route

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/resources", resourceRoutes);
 app.use("/api/projects", projectRoutes);
 
+app.get("/", (req, res) => {
+  res.json("Sprint deployed");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
